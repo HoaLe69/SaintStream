9332270d434a56e287700d9046f48e0d4380c499
HoaLe69 <hoabanhanh@gmail.com>
2024-02-24 15:38:03 +0700

    sign out

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -2,16 +2,26 @@ import Image from 'next/image'
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
 import { Button } from './button'
 import { useSession } from 'next-auth/react'
+import {
+  BookmarkIcon,
+  ArrowLeftStartOnRectangleIcon
+} from '@heroicons/react/24/outline'
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
 
 export default function UserProfile() {
   const session = useSession()
   if (!session?.data?.user) {
-    return <Button primary>Login</Button>
+    return (
+      <Button primary>
+        <Link href="/api/auth/signin">Login</Link>
+      </Button>
+    )
   }
   const fallback =
     'https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png'
   return (
-    <div className="flex items-center gap-2 ml-3">
+    <div className="group flex items-center gap-2 ml-3 relative">
       <Image
         src={session?.data?.user?.image || fallback}
         alt={session?.data?.user?.name || fallback}
@@ -22,6 +32,29 @@ export default function UserProfile() {
       <span className="cursor-pointer">
         <ChevronDownIcon className="w-6 h-6" />
       </span>
+      <SubMenu />
+    </div>
+  )
+}
+
+function SubMenu() {
+  return (
+    <div className="absolute w-[200px] bg-gray-900 transition-all h-0 group-hover:h-[100px] top-full right-0 overflow-hidden mt-2">
+      <ul className="py-2">
+        <li className="p-2 hover:opacity-90 hover:bg-gray-600 cursor-pointer flex gap-1 items-center">
+          <BookmarkIcon className="w-6 h-6" />
+          Your Bookmark
+        </li>
+        <li
+          onClick={async () => {
+            await signOut()
+          }}
+          className="p-2 hover:opacity-90 hover:bg-gray-600 cursor-pointer flex gap-1 items-center"
+        >
+          <ArrowLeftStartOnRectangleIcon className="w-6 h-6" />
+          Sign out
+        </li>
+      </ul>
     </div>
   )
 }
